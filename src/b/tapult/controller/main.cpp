b4860f3f9380d6ca562cbfee9eca99481f8da73c
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-18 00:25:37 +0900

    Refactor pin assignments and comments in main.cpp

--- src/b/tapult/controller/main.cpp
+++ src/b/tapult/controller/main.cpp
@@ -2,37 +2,40 @@
 #include <Arduino.h>
 #include <components/ims/ImSender.h>
 
-// 使用可能なピン: PD3、PD4、PD5、PD6、PD7、PB0、PC0、PC1、PC2、PC3、PC4、PC5
-#define HOOK_PIN PD3
-#define RELEASE_PIN PD4
+// 使用可能なピン定数；
+// PIN_PD3、PIN_PD4、PIN_PD5、PIN_PD6、PIN_PD7、PIN_PB0、PIN_PB1、PIN_PB2、
+// PIN_PC0、PIN_PC1、PIN_PC2、PIN_PC3、PIN_PC4、PIN_PC5
 /*
-#define FORWARD_L_PIN PD5
-#define REVERSE_L_PIN PD6
-#define FORWARD_R_PIN PD7
-#define REVERSE_R_PIN PB0
+#define FORWARD_L_PIN PIN_PD3
+#define REVERSE_L_PIN PIN_PD4
+#define FORWARD_R_PIN PIN_PD5
+#define REVERSE_R_PIN PIN_PD6
 */
+#define HOOK_PIN PIN_PD7
+#define RELEASE_PIN PIN_PB0
 
 void setup() {
-  pinMode(HOOK_PIN, INPUT_PULLUP);
-  pinMode(RELEASE_PIN, INPUT_PULLUP);
+  // 各ピンをINPUT_PULLUPに設定する。これにより、ピンがHIGHになる。
   /*
   pinMode(FORWARD_L_PIN, INPUT_PULLUP);
   pinMode(REVERSE_L_PIN, INPUT_PULLUP);
   pinMode(FORWARD_R_PIN, INPUT_PULLUP);
   pinMode(REVERSE_R_PIN, INPUT_PULLUP);
   */
+  pinMode(HOOK_PIN, INPUT_PULLUP);
+  pinMode(RELEASE_PIN, INPUT_PULLUP);
 }
 
 void loop() {
   static Controller controller;
-  controller.hook = !digitalRead(HOOK_PIN);
-  controller.unhook = !digitalRead(RELEASE_PIN);
   /*
   controller.forwardL = !digitalRead(FORWARD_L_PIN);
   controller.reverseL = !digitalRead(REVERSE_L_PIN);
   controller.forwardR = !digitalRead(FORWARD_R_PIN);
   controller.reverseR = !digitalRead(REVERSE_R_PIN);
   */
+  controller.hook = !digitalRead(HOOK_PIN);
+  controller.unhook = !digitalRead(RELEASE_PIN);
 
   static ImSender imSender(Serial);
   imSender.send(controller);
