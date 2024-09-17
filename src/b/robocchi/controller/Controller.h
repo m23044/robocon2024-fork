b052f867b92223f1c9b1e71d1a2e9fb7c08bf43b
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-18 00:18:02 +0900

    デバッグをしやすくするために他のコントローラーの順番に似せた

--- src/b/robocchi/controller/Controller.h
+++ src/b/robocchi/controller/Controller.h
@@ -4,8 +4,8 @@
 // コントローラーの構造体
 struct Controller {
   unsigned int forwardL : 1;
-  unsigned int forwardR : 1;
   unsigned int reverseL : 1;
+  unsigned int forwardR : 1;
   unsigned int reverseR : 1;
   unsigned int armLevelL : 3; // 0b111 = 7, 0b11 = 3
   unsigned int armLevelR : 3;
--- src/b/tapult/controller/Controller.h
+++ src/b/tapult/controller/Controller.h
@@ -2,12 +2,12 @@
 #define CONTROLLER_H
 
 struct Controller {
-  unsigned int hook : 1;
-  unsigned int unhook : 1;
   unsigned int forwardL : 1;
   unsigned int reverseL : 1;
   unsigned int forwardR : 1;
   unsigned int reverseR : 1;
+  unsigned int hook : 1;
+  unsigned int unhook : 1;
 };
 
 #endif // CONTROLLER_H
