cfeee23ea752daf4a77afc2f3ba7e51faae1dc41
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-18 00:25:05 +0900

    Refactor pin assignments in main.cpp

--- src/b/robocchi/controller/main.cpp
+++ src/b/robocchi/controller/main.cpp
@@ -6,10 +6,10 @@
 // 使用可能なピン定数；
 // PIN_PD3、PIN_PD4、PIN_PD5、PIN_PD6、PIN_PD7、PIN_PB0、PIN_PB1、PIN_PB2、
 // PIN_PC0、PIN_PC1、PIN_PC2、PIN_PC3、PIN_PC4、PIN_PC5
-#define REVERSE_L_PIN PIN_PD3
-#define FORWARD_L_PIN PIN_PD4
-#define REVERSE_R_PIN PIN_PD5
-#define FORWARD_R_PIN PIN_PD6
+#define FORWARD_L_PIN PIN_PD3
+#define REVERSE_L_PIN PIN_PD4
+#define FORWARD_R_PIN PIN_PD5
+#define REVERSE_R_PIN PIN_PD6
 #define ARM_CATCH_L_PIN PIN_PD7
 #define ARM_RELEASE_L_PIN PIN_PB0
 #define ARM_CATCH_R_PIN PIN_PB1
@@ -18,10 +18,10 @@
 #define JUMP_READY_PIN PIN_PC1
 
 void setup() {
-  pinMode(REVERSE_L_PIN, INPUT_PULLUP);
   pinMode(FORWARD_L_PIN, INPUT_PULLUP);
-  pinMode(REVERSE_R_PIN, INPUT_PULLUP);
+  pinMode(REVERSE_L_PIN, INPUT_PULLUP);
   pinMode(FORWARD_R_PIN, INPUT_PULLUP);
+  pinMode(REVERSE_R_PIN, INPUT_PULLUP);
   pinMode(ARM_CATCH_L_PIN, INPUT_PULLUP);
   pinMode(ARM_RELEASE_L_PIN, INPUT_PULLUP);
   pinMode(ARM_CATCH_R_PIN, INPUT_PULLUP);
@@ -33,8 +33,8 @@ void setup() {
 void loop() {
   static Controller controller;
   controller.forwardL = !digitalRead(FORWARD_L_PIN);
-  controller.forwardR = !digitalRead(FORWARD_R_PIN);
   controller.reverseL = !digitalRead(REVERSE_L_PIN);
+  controller.forwardR = !digitalRead(FORWARD_R_PIN);
   controller.reverseR = !digitalRead(REVERSE_R_PIN);
   controller.jump = !digitalRead(JUMP_PIN);
   controller.jumpReady = !digitalRead(JUMP_READY_PIN);
