9619fd9507e9053182b7fcbf17676f9ab31c2326
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-19 17:37:41 +0900

    Add digitalWriteFast library for faster pin operations

--- src/a/child/controller/main.cpp
+++ src/a/child/controller/main.cpp
@@ -2,6 +2,7 @@
 #include "Controller.h" // 同じディレクトリにあるController.hを取得
 #include <Arduino.h> // Arduinoの基本機能を使うためのライブラリ
 #include <components/ims/ImSender.h> // liboshima(大島商船用ライブラリ)のImSender.hを取得
+#include <digitalWriteFast.h> // digitalWriteFast.hを取得
 
 // #defineでピン番号に別名をつける
 // 使用可能なピン定数；
@@ -20,40 +21,41 @@
 // 1度だけ実行される
 void setup() {
   // 各ピンに対しボタンとして使うための設定を行う
-  pinMode(FORWARD_L_PIN, INPUT_PULLUP);
-  pinMode(REVERSE_L_PIN, INPUT_PULLUP);
-  pinMode(FORWARD_R_PIN, INPUT_PULLUP);
-  pinMode(REVERSE_R_PIN, INPUT_PULLUP);
-  pinMode(HOLD_PIN, INPUT_PULLUP);
-  pinMode(RELEASE_PIN, INPUT_PULLUP);
-  pinMode(PUSH_PIN, INPUT_PULLUP);
-  pinMode(PULL_PIN, INPUT_PULLUP);
+  pinModeFast(FORWARD_L_PIN, INPUT_PULLUP);
+  pinModeFast(REVERSE_L_PIN, INPUT_PULLUP);
+  pinModeFast(FORWARD_R_PIN, INPUT_PULLUP);
+  pinModeFast(REVERSE_R_PIN, INPUT_PULLUP);
+  pinModeFast(HOLD_PIN, INPUT_PULLUP);
+  pinModeFast(RELEASE_PIN, INPUT_PULLUP);
+  pinModeFast(PUSH_PIN, INPUT_PULLUP);
+  pinModeFast(PULL_PIN, INPUT_PULLUP);
 }
 
 // 繰り返し実行される
 void loop() {
   // コントローラー変数を作成
   // int a; と同じ意味
-  Controller controller; //Controller複数の変数でできている
+  Controller controller; // Controller複数の変数でできている
 
   // コントローラーの状態を読み取る
   // controller変数は変数の集まりで、各変数には「.」を使ってアクセスする
-  // 『!digitalRead(HOLD_PIN)』はボタンが押されている(いない)時に1(0)になる
-  controller.forwardL = !digitalRead(FORWARD_L_PIN); //controllerのforwardLにアクセスする
-  controller.reverseL = !digitalRead(REVERSE_L_PIN);
-  controller.forwardR = !digitalRead(FORWARD_R_PIN);
-  controller.reverseR = !digitalRead(REVERSE_R_PIN);
-  controller.hold = !digitalRead(HOLD_PIN);
-  controller.release = !digitalRead(RELEASE_PIN);
-  controller.push = !digitalRead(PUSH_PIN);
-  controller.pull = !digitalRead(PULL_PIN);
+  // 『!digitalReadFast(HOLD_PIN)』はボタンが押されている(いない)時に1(0)になる
+  controller.forwardL =
+      !digitalReadFast(FORWARD_L_PIN); // controllerのforwardLにアクセスする
+  controller.reverseL = !digitalReadFast(REVERSE_L_PIN);
+  controller.forwardR = !digitalReadFast(FORWARD_R_PIN);
+  controller.reverseR = !digitalReadFast(REVERSE_R_PIN);
+  controller.hold = !digitalReadFast(HOLD_PIN);
+  controller.release = !digitalReadFast(RELEASE_PIN);
+  controller.push = !digitalReadFast(PUSH_PIN);
+  controller.pull = !digitalReadFast(PULL_PIN);
 
   // IM920SLの送信機の変数を作成
   // staticはその変数が関数の呼び出しのたびに初期化されないようにする
   // つまりこの場合、sender変数は1度だけ初期化される
-  static ImSender sender(Serial); //ImSender型のsender変数を宣言する
+  static ImSender sender(Serial); // ImSender型のsender変数を宣言する
   // コントローラーの状態を送信
-  sender.send(controller); //imでコントローラーの状態を送信している
+  sender.send(controller); // imでコントローラーの状態を送信している
   // 60ミリ秒待機
   delay(IM_SEND_INTERVAL); // delay(60);
 }
