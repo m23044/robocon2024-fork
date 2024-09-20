f9c2290282d8ee2f9c0733363520adb938acdaf3
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-20 18:52:40 +0900

    Refactor Controller struct to add padding and increase arm level range

--- src/b/robocchi/controller/Controller.h
+++ src/b/robocchi/controller/Controller.h
@@ -6,15 +6,16 @@ struct Controller {
   unsigned int forwardL : 1;
   unsigned int reverseL : 1;
   unsigned int forwardR : 1;
-  unsigned int reverseR : 1;
-  unsigned int armLevelL : 3; // 0b111 = 7, 0b11 = 3
-  unsigned int armLevelR : 3;
+  unsigned int reverseR : 1;  
   unsigned int jump : 1;
   unsigned int jumpReady : 1;
+  unsigned int padding1 : 2; // 2ビットのパディング
+  unsigned int armLevelL : 4; // 0b111 = 7, 0b11 = 3
+  unsigned int armLevelR : 4;
 
-  // サーボの値を0から3の範囲に制限するため
+  // サーボの値を0から5の範囲に制限するため
   void incrementArmLevelL() {
-    if (armLevelL < 3)
+    if (armLevelL < 4)
       armLevelL++;
   }
   void decrementArmLevelL() {
@@ -23,7 +24,7 @@ struct Controller {
   }
 
   void incrementArmLevelR() {
-    if (armLevelR < 3)
+    if (armLevelR < 4)
       armLevelR++;
   }
   void decrementArmLevelR() {
