96d34658c4a586058dd24411faef8332612748c8
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-30 20:02:49 +0900

    Refactor main.cpp to improve readbility

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -21,6 +21,34 @@ IM920SL im(serial);
 // 接続中のランプを消灯する関数
 void onDisconnected() { digitalWriteFast(CONNECT_LED_PIN, LOW); }
 
+// ロボットから返答があった時に呼び出される関数
+void serialEvent() {
+  // ランプを消灯
+  digitalWriteFast(CONNECT_LED_PIN, HIGH);
+  // タイマーのカウントを最初からやり直す
+  MsTimer2::start();
+}
+
+uint16_t generateKeyCode() {
+  // ボタンの状態を格納する変数(16ビット)を宣言
+  // 0000 0000 0000 0000 <- なんのボタンも押されていない状態
+  // 0000 0000 0000 0001 <- btnPins[0]のボタンが押されている状態
+  // 1000 0000 0000 0001 <- btnPins[0]とbtnPins[15]のボタンが押されている状態
+  // ^^^^ ^^^^ ^^^^ ^^^^
+  // |||| |||| |||| ||||
+  // |||| |||| |||| |||+---- btnPins[0]の状態
+  // |||| |||| |||| ||+----- btnPins[1]の状態
+  // |||| |||| |||| |+------ btnPins[2]の状態
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  // +---------------------- btnPins[15]の状態
+  uint16_t keyCode = 0;
+
+  for (uint8_t i = 0; i < sizeof(btnPins); i++) {
+    keyCode |= !digitalReadFast(btnPins[i]) << i;
+  }
+  return keyCode;
+}
+
 // 1度だけ実行される
 void setup() {
   // 各ピンに対しボタンとして使うための設定を行う
@@ -40,35 +68,10 @@ void setup() {
 
 // 繰り返し実行される
 void loop() {
-  // ボタンの状態を格納する変数(16ビット)を宣言
-  // 0000 0000 0000 0000 <- なんのボタンも押されていない状態
-  // 0000 0000 0000 0001 <- btnPins[0]のボタンが押されている状態
-  // 1000 0000 0000 0001 <- btnPins[0]とbtnPins[15]のボタンが押されている状態
-  // ^^^^ ^^^^ ^^^^ ^^^^
-  // |||| |||| |||| ||||
-  // |||| |||| |||| |||+---- btnPins[0]の状態
-  // |||| |||| |||| ||+----- btnPins[1]の状態
-  // |||| |||| |||| |+------ btnPins[2]の状態
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // +---------------------- btnPins[15]の状態
-  uint16_t keyCode = 0;
-
-  // ボタンの状態をビット演算で読み取る
-  for (uint8_t i = 0; i < sizeof(btnPins); i++) {
-    // ボタンが押されているとき1を設定
-    keyCode |= !digitalReadFast(btnPins[i]) << i;
-  }
-
+  // ボタンの状態を取得し、キーコードを生成
+  uint16_t keyCode = generateKeyCode();
   // コントローラーの状態をIM920SLを使って送信
   im.send(keyCode);
   // 100ミリ秒待機
   delay(IM_SEND_INTERVAL);
-}
-
-// ロボットから返答があった時に呼び出される関数
-void serialEvent() {
-  // ランプを消灯
-  digitalWriteFast(CONNECT_LED_PIN, HIGH);
-  // タイマーのカウントを最初からやり直す
-  MsTimer2::start();
 }
