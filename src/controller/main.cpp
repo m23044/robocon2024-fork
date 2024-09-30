0ab49390ba9c2df7645f81278fcc878d4df9c212
SoshiroFujimori <oshima.k.kondate@gmail.com>
2024-09-30 19:04:00 +0900

    simplify

--- src/controller/main.cpp
+++ src/controller/main.cpp
@@ -1,40 +1,24 @@
 // 他のファイルのプログラムを取得する
-#include "Controller.h" // 同じディレクトリにあるController.hを取得
 #include <Arduino.h>
 #include <MsTimer2.h> // MsTimer2.hを取得
 #include <components/ims/IM920SL.h> // liboshima(大島商船用ライブラリ)のIM920SL.hを取得
 #include <digitalWriteFast.h> // digitalWriteFast.hを取得
 
 // #defineでピン番号に別名をつける
-// 使用可能なピン定数；
-// PIN_PD3、PIN_PD4、PIN_PD5、PIN_PD6、PIN_PD7、PIN_PB0、PIN_PB1、PIN_PB2、
-// PIN_PC0、PIN_PC1、PIN_PC2、PIN_PC3、PIN_PC4、PIN_PC5
-#define BTN1_PIN PIN_PD3 // PIN_PD3ピンをBTN1_PINとして定義
-#define BTN2_PIN PIN_PD4
-#define BTN3_PIN PIN_PD5
-#define BTN4_PIN PIN_PD6
-#define BTN5_PIN PIN_PD7
-#define BTN6_PIN PIN_PB0
-#define BTN7_PIN PIN_PB1
-#define BTN8_PIN PIN_PB2
-#define BTN9_PIN PIN_PC2
-#define BTN10_PIN PIN_PC3
-#define BTN11_PIN PIN_PC4
-#define BTN12_PIN PIN_PC5
 #define CONNECT_LED_PIN PIN_PC0 // 受信中のランプのピン番号
 
+// ボタンピン番号の配列
+const uint8_t btnPins[] = {PIN_PD3, PIN_PD4, PIN_PD5, PIN_PD6,
+                           PIN_PD7, PIN_PB0, PIN_PB1, PIN_PB2,
+                           PIN_PC2, PIN_PC3, PIN_PC4, PIN_PC5};
+
 // SerialPort型のserial変数を宣言し、Serial変数で初期化する
-// やっていることは、int a = 1; と同じ
-// イメージ：SerialPort serial = Serial;
 SerialPort serial(Serial);
 
 // ImSender型のsender変数を宣言し、serial変数で初期化する
-// やっていることは、int a = 1; と同じ
-// イメージ：IM920SL im = serial;
 IM920SL im(serial);
 
 // 緊急停止のランプを点灯させる関数
-// 500ミリ秒経過すると、Timer1によって呼び出される
 void emergencyStop() {
   digitalWriteFast(CONNECT_LED_PIN, HIGH); // ランプを点灯
 }
@@ -42,18 +26,9 @@ void emergencyStop() {
 // 1度だけ実行される
 void setup() {
   // 各ピンに対しボタンとして使うための設定を行う
-  pinModeFast(BTN1_PIN, INPUT_PULLUP);
-  pinModeFast(BTN2_PIN, INPUT_PULLUP);
-  pinModeFast(BTN3_PIN, INPUT_PULLUP);
-  pinModeFast(BTN4_PIN, INPUT_PULLUP);
-  pinModeFast(BTN5_PIN, INPUT_PULLUP);
-  pinModeFast(BTN6_PIN, INPUT_PULLUP);
-  pinModeFast(BTN7_PIN, INPUT_PULLUP);
-  pinModeFast(BTN8_PIN, INPUT_PULLUP);
-  pinModeFast(BTN9_PIN, INPUT_PULLUP);
-  pinModeFast(BTN10_PIN, INPUT_PULLUP);
-  pinModeFast(BTN11_PIN, INPUT_PULLUP);
-  pinModeFast(BTN12_PIN, INPUT_PULLUP);
+  for (uint8_t i = 0; i < sizeof(btnPins); i++) {
+    pinModeFast(btnPins[i], INPUT_PULLUP);
+  }
   // ランプを出力に設定
   pinModeFast(CONNECT_LED_PIN, OUTPUT);
 
@@ -67,28 +42,17 @@ void setup() {
 
 // 繰り返し実行される
 void loop() {
-  // コントローラー変数を作成
-  // int a; と同じ意味
-  Controller controller;
+  // コントローラーの状態をビットで管理
+  uint16_t keyCode = 0;
 
-  // コントローラーの状態を読み取る
-  // controller変数は変数の集まりで、各変数には「.」を使ってアクセスする
-  // 『!digitalReadFast(ACTIONn_n_PIN)』はボタンが押されている(いない)時に1(0)になる
-  controller.btn1 = !digitalReadFast(BTN1_PIN);
-  controller.btn2 = !digitalReadFast(BTN2_PIN);
-  controller.btn3 = !digitalReadFast(BTN3_PIN);
-  controller.btn4 = !digitalReadFast(BTN4_PIN);
-  controller.btn5 = !digitalReadFast(BTN5_PIN);
-  controller.btn6 = !digitalReadFast(BTN6_PIN);
-  controller.btn7 = !digitalReadFast(BTN7_PIN);
-  controller.btn8 = !digitalReadFast(BTN8_PIN);
-  controller.btn9 = !digitalReadFast(BTN9_PIN);
-  controller.btn10 = !digitalReadFast(BTN10_PIN);
-  controller.btn11 = !digitalReadFast(BTN11_PIN);
-  controller.btn12 = !digitalReadFast(BTN12_PIN);
+  // ボタンの状態をビット演算で読み取る
+  for (uint8_t i = 0; i < sizeof(btnPins); i++) {
+    // ボタンが押されているとき1を設定
+    keyCode |= !digitalReadFast(btnPins[i]) << i;
+  }
 
   // コントローラーの状態をIM920SLを使って送信
-  im.send(controller);
+  im.send(keyCode);
   // 100ミリ秒待機
   delay(IM_SEND_INTERVAL);
 }
--- src/robot/main.cpp
+++ src/robot/main.cpp
@@ -1,52 +1,26 @@
-// 他のファイルからプログラムを取得する
-#include "controller/Controller.h" // 一つ上のディレクトリのcontrollerディレクトリのController.hを取得
-#include <Arduino.h>               // Arduino.hを取得
-#include <MsTimer2.h>              // MsTimer2.hを取得
-#include <Servo.h> // サーボモータを使うためのライブラリ
-#include <components/ims/IM920SL.h> // liboshima(大島商船用ライブラリ)のIM920SL.hを取得
-#include <components/motors/NonSpeedAdjustable.h> // liboshimaのNonSpeedAdjustable.hを取得
+#include <Arduino.h>
+#include <MsTimer2.h>
+#include <Servo.h>
+#include <components/ims/IM920SL.h>
+#include <components/motors/NonSpeedAdjustable.h>
 
-/*
-  NonSpeedAdjustable型の変数を定義している。
-  NonSpeedAdjustable型はint型と違い複雑な型であるため、int型の様に数字1つで初期化することはできない。
+NonSpeedAdjustable actions[] = {
+    NonSpeedAdjustable(PIN_PD2, PIN_PD4), NonSpeedAdjustable(PIN_PD7, PIN_PB0),
+    NonSpeedAdjustable(PIN_PC3, PIN_PC2), NonSpeedAdjustable(PIN_PC1, PIN_PC0),
+    NonSpeedAdjustable(PIN_PC4, PIN_PC5)};
 
-  int i = 1;
-  でint型のi変数を作成し、1で初期化する。
+SerialPort serial(Serial);
+IM920SL im(serial);
 
-  NonSpeedAdjustable action1(PIN_PD2, PIN_PD4);
-  でNonSpeedAdjustable型のaction1変数を作成し、PIN_PD2とPIN_PD4で初期化する。
-
-  ※NonSpeedAdjustableはモータドライバのことで、2つのピンを指定することでモータを制御することができる。※
-*/
-// 使用可能なピン:
-// PIN_PD2、PIN_PD4、PIN_PD7、PIN_PB0、PIN_PC2、PIN_PC3、[PIN_PC0、PIN_PC1]
-NonSpeedAdjustable action1(PIN_PD2, PIN_PD4);
-NonSpeedAdjustable action2(PIN_PD7, PIN_PB0);
-NonSpeedAdjustable action3(PIN_PC3, PIN_PC2);
-NonSpeedAdjustable action4(PIN_PC1, PIN_PC0);
-NonSpeedAdjustable action5(PIN_PC4, PIN_PC5);
-
-SerialPort serial(Serial); // SerialPort serial = Serial;
-IM920SL im(serial);        // IM920SL im = serial;
-Controller controller;     // int a;
-
-// 緊急停止用の関数
-// 500ミリ秒経過すると、Timer1によって呼び出される
 void emergencyStop() {
-  action1.stop();
-  action2.stop();
-  action3.stop();
-  action4.stop();
-  action5.stop();
+  for (auto &action : actions) {
+    action.stop();
+  }
 }
 
-// setup1回だけ実行される
 void setup() {
-  // 500000マイクロ秒(0.5秒)のタイマーを設定
   MsTimer2::set(IM_RECEIVE_INTERVAL_MILLIS, emergencyStop);
   MsTimer2::start();
-
-  // IM920SLの初期化
   im.begin();
 }
 
@@ -55,82 +29,17 @@ void loop() {}
 void serialEvent() {
   MsTimer2::start();
 
-  // コントローラーの変更を読み込む
-  im.receive(controller);
+  uint16_t keyCode;
+  im.receive(keyCode);
 
-  // もしaction1_1ボタンが押されていたら
-  if (controller.btn1) {
-    // action1を前進させる
-    action1.forward();
+  for (uint8_t i = 0; i < sizeof(actions) / sizeof(actions[0]); i++) {
+    if (keyCode & (1 << i)) {
+      if (i % 2 == 0)
+        actions[i].forward();
+      else
+        actions[i].reverse();
+    } else {
+      actions[i].stop();
+    }
   }
-  // もしaction1_2ボタンが押されていたら
-  else if (controller.btn2) {
-    // action1を後進させる
-    action1.reverse();
-  }
-  // それ以外の場合
-  else {
-    // action1を停止させる
-    action1.stop();
-  }
-
-  // もしaction2_1ボタンが押されていたら
-  if (controller.btn3) {
-    // action2を前進させる
-    action2.forward();
-  }
-  // もしaction2_2ボタンが押されていたら
-  else if (controller.btn4) {
-    // action2を後進させる
-    action2.reverse();
-  }
-  // それ以外の場合
-  else {
-    // action2を停止させる
-    action2.stop();
-  }
-
-  // もしaction3_1ボタンが押されていたら
-  if (controller.btn5) {
-    // action3を前進させる
-    action3.forward();
-  }
-  // もしaction3_2ボタンが押されていたら
-  else if (controller.btn6) {
-    // action3を後進させる
-    action3.reverse();
-  }
-  // それ以外の場合
-  else {
-    // action3を停止させる
-    action3.stop();
-  }
-
-  // もしaction5_1ボタンが押されていたら
-  if (controller.btn7) {
-    // action4を前進させる
-    action4.forward();
-  }
-  // もしaction5_2ボタンが押されていたら
-  else if (controller.btn8) {
-    // action4を後進させる
-    action4.reverse();
-  }
-  // それ以外の場合
-  else {
-    // action4を停止させる
-    action4.stop();
-  }
-
-  if (controller.btn9) {
-    action5.forward();
-  } else if (controller.btn10) {
-    action5.reverse();
-  } else {
-    action5.stop();
-  }
-
-  // コントローラーに受信が完了したことを知らせる
-  // im.send("Succes");
-  // Serial.println("TXDA 1111");
 }
